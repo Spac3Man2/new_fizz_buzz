36b3b3be1e10d1de345bc27506845a564cf39b3d
Spac3Man2 <Spac3Man2>
2017-03-01 22:33:54 -0500

    new_fizz_buzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizz_buzz_kata(num)
+   num
+end	
+
+
+# # def fizz_buzz_kata(array)
+# #    100.times do
+#    end
+# end	
---
+++ test_fizz_buzz.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require_relative 'fizzbuzz.rb'
+
+class Test_fizz_buzz < Minitest::Test
+
+
+	def test_1_returns_1
+		assert_equal(1, fizz_buzz_kata(1))
+	end
+
+	# def test_2_returns_2
+	# 	assert_equal(2, fizz_buzz_kata(2))
+ #    end
+
+end    
+
+#     def test_3_returns_mined
+#     	assert_equal(3, fizz_buzz_kata("mined"))
+#     end	
+# end
+
+
+
+
+# require 'minitest/autorun'
+# require_relative 'fizzbuzz.rb'
+
+# class Test_fizz_buzz < Minitest::Test
+ 	
+#  	def test_returns_array_of_100_numbers
+#         assert_equal(100, fizz_buzz_kata(1..100))
+# 	end  
+# end		
