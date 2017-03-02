105648e29d89987eb4c1a092761fea09dafd4548
Spac3Man2 <Spac3Man2>
2017-03-01 22:38:21 -0500

    new_fizz_buzz

--- test_fizz_buzz.rb
+++ test_fizz_buzz.rb
@@ -5,18 +5,21 @@ class Test_fizz_buzz < Minitest::Test
 
 
 	def test_1_returns_1
+		num = 1
 		assert_equal(1, fizz_buzz_kata(1))
 	end
 
-	def test_2_returns_2
-		assert_equal(2, fizz_buzz_kata(2))
-    end
+# 	def test_2_returns_2
+# 		num = 2
+# 		assert_equal(2, fizz_buzz_kata(2))
+#     end
 
-end    
+# end    
 
-#     def test_3_returns_mined
-#     	assert_equal(3, fizz_buzz_kata("mined"))
-#     end	
+#     def test_3_returns_mined 
+#         num = 3
+#        	assert_equal(3, fizz_buzz_kata("mined"))
+    end	
 # end
 
 
